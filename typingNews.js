4498a73c7933d21c62d14ddc50ca9068752a3e93
hecthor <127270805+hecthor2007@users.noreply.github.com>
2025-06-01 18:21:15 -0400

    header y api
    todavia te falta mucho con la api y ordena el codigo

--- typingNews.js
+++ typingNews.js
@@ -5,6 +5,19 @@ const state = document.getElementById("state");
 var goal = "Esta es una prueba para el texto objetivo, despues ira aqui una noticia";
 var startTimer = null;
 
+const apiKey = "beced55d853b46769ce4a2b96bddc744";
+const url = `https://newsapi.org/v2/top-headlines?country=us&pageSize=5&apiKey=${apiKey}`;
+
+function fetchNews()
+{
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        goal = data.articles[0].title;
+        textVisualFormat("");
+    });
+}
+
 function textVisualFormat (userInput)
 {
 let visualText = "";
@@ -16,15 +29,15 @@ for(let i = 0; i < goal.length; i++)
 
     if(userLetter === undefined)
     {
-        visualText +=`<span class="pendiente">${goalLetter}</span>`;
+        visualText +=`<span class="pending">${goalLetter}</span>`;
     }
     else if(userLetter === goalLetter)
     {
-        visualText += `<span class="correcta">${goalLetter}</span>`;
+        visualText += `<span class="correct">${goalLetter}</span>`;
     }
     else
     {
-        visualText += `<span class="incorrecta">${goalLetter}</span>`;
+        visualText += `<span class="incorrect">${goalLetter}</span>`;
     }
 }
 textGoal.innerHTML = visualText;
@@ -51,6 +64,7 @@ function resetTyping() {
     wpmDisplay.textContent = "WPM: 0";
     textVisualFormat("");
     typeZone.focus();
+    goal = data.articles[requestCont].title;
 }
 
 typeZone.addEventListener
@@ -69,7 +83,7 @@ typeZone.addEventListener
     if (userText.length === goal.length) {
         state.textContent = "finished!! press space to continue";
         typeZone.disabled = true;
-        startTime = null;//termina el contador
+        startTimer = null;//termina el contador
     }
     else
     {
@@ -82,4 +96,6 @@ document.addEventListener("keydown", (event) => {
         resetTyping();
     }
 });
+
+fetchNews();
 textVisualFormat("");
